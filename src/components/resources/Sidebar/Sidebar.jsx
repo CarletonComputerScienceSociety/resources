11cfb23f5aeb14a98b30d9423f010963d7ffbbe8
Matthew M-B <matthew.mb@getjobber.com>
2023-06-27 15:57:10 -0400

    Add sidebar style first pass

--- src/components/resources/Sidebar/Sidebar.jsx
+++ src/components/resources/Sidebar/Sidebar.jsx
@@ -1,12 +1,18 @@
-import SidebarItem from "./SidebarItem"
-import items from "./data/SidebarData.json"
+import SidebarItem from "./SidebarItem";
+import items from "./data/SidebarData.json";
 import "./Sidebar.style.scss";
 
-export default function Sidebar() {    
-    return (                      
-        <div className="sidebar">                                                        
-          { items.map((item, index) => <SidebarItem key={index} item={item} />) }
-        </div>
-    )
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__logo">
+        <a href="/">
+          <img src="/images/ccss-logo-2022.png" alt="logo" />
+        </a>
+      </div>
+      {items.map((item, index) => (
+        <SidebarItem key={index} item={item} />
+      ))}
+    </div>
+  );
 }
-
