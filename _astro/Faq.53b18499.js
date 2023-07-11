482ce00968e737306a0e18edbb886f7947a1d6aa
MathyouMB <MathyouMB@users.noreply.github.com>
2023-07-11 18:37:08 +0000

    deploy: 3a76dbd62f853544a8eb86ba872aee38e37a0f6e

--- _astro/Faq.53b18499.js
+++ _astro/Faq.53b18499.js
@@ -1,4 +1,4 @@
-import{c as g,a as $,r as a,b as t,m as u,f as v,e as _,F as d,d as i,h}from"./resources.8d6881aa.js";/* empty css                                  */import"./jsx-runtime.95f74388.js";import{$ as q}from"./Base.35b27d05.js";/* empty css                           */const F=g("https://resources.carletoncomputerscience.ca"),b=$(async(o,p,c)=>{const n=o.createAstro(F,p,c);n.self=b;const{frontmatter:e}=n.props,l={title:e.title,description:e.short_answer,url:"",image:"/"};return a`${t(o,"Base",q,{metadata:l},{default:s=>a`
+import{c as g,a as $,r as a,b as t,m as u,f as v,e as _,F as d,d as i,h}from"./resources.70e8a23d.js";/* empty css                                  */import"./jsx-runtime.95f74388.js";import{$ as q}from"./Base.975cead9.js";/* empty css                           */const F=g("https://resources.carletoncomputerscience.ca"),b=$(async(o,p,c)=>{const n=o.createAstro(F,p,c);n.self=b;const{frontmatter:e}=n.props,l={title:e.title,description:e.short_answer,url:"",image:"/"};return a`${t(o,"Base",q,{metadata:l},{default:s=>a`
   ${u(s)}<div class="faq-page">
     <div class="faq-page__body">
       <div class="faq-page__card">
---
+++ _astro/the-value-of-grades.6de0c186.js
@@ -0,0 +1,35 @@
+const e="academics/the-value-of-grades.md",r="resources",a="academics/what-is-impacted-by-your-grades",n=`
+## Scholarships
+
+In order to maintain your entrance scholarships of **CGPA of 10.0** is required. It's very likely other internal and external scholarships will be dependant on you maintainng a certain GPA.
+
+## Course Prerequisites
+
+Some courses require you receive a **minimum grade above 50%** to register for future courses. Ex: A grade of **C-** or above in COMP 1805 is required in order to take COMP 2804.
+
+## Eligibility for Co-op
+
+In order to be eligible to participate in the Co-op program, you must maintain a **CGPA of 8.00**.
+
+## Co-op Grade Submissions
+
+As part of each Co-p job application, you will be required to submit your grades with your application. Employers may use this filter candidates.
+
+## Qualifying for DSRI
+
+In order to qualify for [DSRI](/careers/carleton-opportunities/dsri/), you must maintain a **GPA of 10.0**.
+
+## Grad School Applications
+
+Grades are very important when applying to grad school and other post-undergrad academic pursuits.
+`,o={title:"What is Impacted By Your Grades",date:new Date(16890336e5)},t={type:"content",filePath:"/home/runner/work/resources/resources/src/content/resources/academics/the-value-of-grades.md",rawData:`
+layout: ../../../layouts/Resource.astro
+title: What is Impacted By Your Grades
+date: 2023-07-11T00:00:00.000Z
+draft: false
+slug: academics/what-is-impacted-by-your-grades
+sources:
+  - name: Co-operative Education - Requirements by Program
+    link: https://calendar.carleton.ca/undergrad/regulations/co-operativeeducation/
+  - name: Faculty of Science - Dean's Summer Research Internships
+    link: https://science.carleton.ca/students/undergraduate-resources/deans-summer-research-internships/`};export{t as _internal,n as body,r as collection,o as data,e as id,a as slug};
