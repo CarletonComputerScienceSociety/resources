01787e84633d46e20aa179f1fdae7f1e4e57ef29
MathyouMB <MathyouMB@users.noreply.github.com>
2023-07-11 17:50:11 +0000

    deploy: 03da5ec4954d76abd2dddd4b20c1dfa8a7d5ae89

--- _astro/Faq.9f2c59e8.js
+++ _astro/Faq.9f2c59e8.js
@@ -1,4 +1,4 @@
-import{c as g,a as $,r as a,b as t,m as u,e as v,d as _,F as d,f as i,h}from"./resources.2f5e5c37.js";/* empty css                                  */import"./jsx-runtime.95f74388.js";import{$ as q}from"./Base.ada76f2f.js";/* empty css                           */const F=g("https://resources.carletoncomputerscience.ca"),b=$(async(o,p,c)=>{const n=o.createAstro(F,p,c);n.self=b;const{frontmatter:e}=n.props,l={title:e.title,description:e.short_answer,url:"",image:"/"};return a`${t(o,"Base",q,{metadata:l},{default:s=>a`
+import{c as g,a as $,r as a,b as t,m as u,f as v,e as _,F as d,d as i,h}from"./resources.dbf74d60.js";/* empty css                                  */import"./jsx-runtime.95f74388.js";import{$ as q}from"./Base.489c6f4a.js";/* empty css                           */const F=g("https://resources.carletoncomputerscience.ca"),b=$(async(o,p,c)=>{const n=o.createAstro(F,p,c);n.self=b;const{frontmatter:e}=n.props,l={title:e.title,description:e.short_answer,url:"",image:"/"};return a`${t(o,"Base",q,{metadata:l},{default:s=>a`
   ${u(s)}<div class="faq-page">
     <div class="faq-page__body">
       <div class="faq-page__card">
---
+++ _astro/dsri.fabc42d3.js
@@ -0,0 +1,19 @@
+const e="careers/carleton-opportunies/dsri.md",s="resources",r="careers/carleton-opportunities/dsri",t=`
+Dean’s Summer Research Internships **(DSRIs)** allow students the opportunity to work with professors to conduct research. To be eligible, students must have **first-year** standing and be enrolled full-time, with an overall **CGPA of 10.0** or higher at the end of their first term of study.
+
+Eligible students are e-mailed an invitation to apply, with a **mid-February** submission deadline. The DSRI application requires students to discuss why they want to do research, to identify the professor they wish to work with, explain why they want to join the professor’s research program, and describe what they will work on.
+
+For more information, [click here](https://science.carleton.ca/students/undergraduate-resources/deans-summer-research-internships/).
+
+## Tips for Getting a DSRI with a COMP Professor
+
+_Please Contribute Tips!_
+`,n={title:"Dean's Summer Research Internships",date:new Date(16890336e5)},o={type:"content",filePath:"/home/runner/work/resources/resources/src/content/resources/careers/carleton-opportunies/dsri.md",rawData:`
+layout: ../../../../layouts/Resource.astro
+title: Dean's Summer Research Internships
+date: 2023-07-11T00:00:00.000Z
+draft: false
+slug: careers/carleton-opportunities/dsri
+sources:
+  - name: Faculty of Science - Dean's Summer Research Internships
+    link: https://science.carleton.ca/students/undergraduate-resources/deans-summer-research-internships/`};export{o as _internal,t as body,s as collection,n as data,e as id,r as slug};
