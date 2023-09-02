8e69c3d116eef53310a94024416eb313f4b78b78
MathyouMB <MathyouMB@users.noreply.github.com>
2023-09-02 21:27:57 +0000

    deploy: c869f73a6274dfb621b3ac189200ac420daa30ae

--- _astro/self-learning.dd9a752f.js
+++
@@ -1,6 +0,0 @@
-const e="self-learning.md",n="resources",t="self-learning",s="",r={title:"Self Learning",date:new Date(16936128e5)},o={type:"content",filePath:"/home/runner/work/resources/resources/src/content/resources/self-learning.md",rawData:`
-layout: ../../layouts/Resource.astro
-title: Self Learning
-date: 2023-09-02T00:00:00.000Z
-draft: false
-slug: self-learning`};export{o as _internal,s as body,n as collection,r as data,e as id,t as slug};
